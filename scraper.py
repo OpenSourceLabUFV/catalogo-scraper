2864aa8701391b97d2c18db1ab0d6ecee473c211
Lucas Joviniano <lucasjoviniano1@gmail.com>
2021-04-28 10:38:49 -0300

    Remove unused import

--- scraper.py
+++ scraper.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pandas as pd
 import os
 import json
 from bs4 import BeautifulSoup
